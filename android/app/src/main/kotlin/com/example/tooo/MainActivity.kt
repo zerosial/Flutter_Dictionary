ad74d7a1e36c17b4674116caa1195a52625e88e1
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-08-27 16:06:54 +0900

    feat: 버전업

---
+++ android/app/src/main/kotlin/com/example/tooo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tooo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# toonflix
+# tooo
 
 A new Flutter project.
 
