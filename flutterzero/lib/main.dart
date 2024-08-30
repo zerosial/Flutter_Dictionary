f908e225533f5bee7395a0a85b8c64f4a036a8e5
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-08-30 10:22:23 +0900

    feat: 1week 딕셔너리 클래스 + 플러터 앱 만들어보기

--- flutterzero/lib/main.dart
+++ flutterzero/lib/main.dart
@@ -1,5 +1,63 @@
 import 'package:flutter/material.dart';
 
+typedef Word = Map<String, String>;
+
+class Dictionary {
+  final Map<String, String> _words = {};
+
+  void add(String term, String definition) {
+    _words[term] = definition;
+  }
+
+  String? get(String term) {
+    return _words[term];
+  }
+
+  void delete(String term) {
+    _words.remove(term);
+  }
+
+  void update(String term, String definition) {
+    if (_words.containsKey(term)) {
+      _words[term] = definition;
+    }
+  }
+
+  void upsert(String term, String definition) {
+    _words[term] = definition;
+  }
+
+  List<String> showAll() {
+    return _words.entries
+        .map((entry) => '${entry.key}: ${entry.value}')
+        .toList();
+  }
+
+  int count() {
+    return _words.length;
+  }
+
+  bool exists(String term) {
+    return _words.containsKey(term);
+  }
+
+  void bulkAdd(List<Word> words) {
+    for (var word in words) {
+      _words[word['term']!] = word['definition']!;
+    }
+  }
+
+  void bulkDelete(List<String> terms) {
+    for (var term in terms) {
+      _words.remove(term);
+    }
+  }
+
+  List<MapEntry<String, String>> getAllWords() {
+    return _words.entries.toList();
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,119 +65,287 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: DictionaryScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DictionaryScreen extends StatefulWidget {
+  const DictionaryScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DictionaryScreenState createState() => _DictionaryScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DictionaryScreenState extends State<DictionaryScreen> {
+  final Dictionary dictionary = Dictionary();
 
-  void _incrementCounter() {
+  // Controllers for adding words
+  final TextEditingController addTermController = TextEditingController();
+  final TextEditingController addDefinitionController = TextEditingController();
+
+  // Controller for searching words
+  final TextEditingController searchTermController = TextEditingController();
+
+  String? searchTerm;
+  String? searchResult;
+  bool isListVisible = false;
+  String feedbackMessage = '';
+
+  void _addWord() {
+    final term = addTermController.text;
+    final definition = addDefinitionController.text;
+    if (term.isNotEmpty && definition.isNotEmpty) {
+      setState(() {
+        dictionary.add(term, definition);
+        feedbackMessage = 'Word added successfully.';
+      });
+      addTermController.clear();
+      addDefinitionController.clear();
+    }
+  }
+
+  void _updateWord() {
+    final term = addTermController.text;
+    final definition = addDefinitionController.text;
+    if (term.isNotEmpty && definition.isNotEmpty) {
+      setState(() {
+        if (dictionary.exists(term)) {
+          dictionary.update(term, definition);
+          feedbackMessage = 'Word updated successfully.';
+        } else {
+          feedbackMessage = 'Word not found.';
+        }
+      });
+    }
+  }
+
+  void _upsertWord() {
+    final term = addTermController.text;
+    final definition = addDefinitionController.text;
+    if (term.isNotEmpty && definition.isNotEmpty) {
+      setState(() {
+        if (dictionary.exists(term)) {
+          feedbackMessage = 'Word updated successfully.';
+        } else {
+          feedbackMessage = 'Word added successfully.';
+        }
+        dictionary.upsert(term, definition);
+      });
+    }
+  }
+
+  void _searchWord() {
+    final term = searchTermController.text;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      searchTerm = term;
+      searchResult = dictionary.get(term);
+    });
+  }
+
+  void _toggleWordList() {
+    setState(() {
+      isListVisible = !isListVisible;
+    });
+  }
+
+  void _deleteWord(String term) {
+    setState(() {
+      dictionary.delete(term);
+      searchTerm = null;
+      searchResult = null;
+      feedbackMessage = 'Word "$term" deleted successfully.';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Dictionary App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 단어 추가 및 업데이트 부분
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Add or Update a Word',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      TextField(
+                        controller: addTermController,
+                        decoration: const InputDecoration(labelText: 'Term'),
+                      ),
+                      TextField(
+                        controller: addDefinitionController,
+                        decoration:
+                            const InputDecoration(labelText: 'Definition'),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: _addWord,
+                            child: const Text('Add Word'),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: _updateWord,
+                            child: const Text('Update Word'),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: _upsertWord,
+                            child: const Text('Upsert Word'),
+                          ),
+                        ],
+                      ),
+                      if (feedbackMessage.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text(
+                            feedbackMessage,
+                            style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                // 단어 검색 부분
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Search a Word',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      TextField(
+                        controller: searchTermController,
+                        decoration:
+                            const InputDecoration(labelText: 'Search Term'),
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: _searchWord,
+                        child: const Text('Search Word'),
+                      ),
+                    ],
+                  ),
+                ),
+                // 검색 결과 표시
+                if (searchTerm != null)
+                  searchResult != null
+                      ? ListTile(
+                          title: Text(
+                            searchTerm!,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            searchResult!,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            color: Colors.red,
+                            onPressed: () {
+                              _deleteWord(searchTerm!);
+                            },
+                          ),
+                        )
+                      : Text(
+                          'Word "$searchTerm" not found.',
+                          style:
+                              const TextStyle(fontSize: 18, color: Colors.red),
+                        ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          // 전체 단어 리스트 표시 (오버레이)
+          if (isListVisible)
+            Positioned.fill(
+              child: Container(
+                color: Colors.white.withOpacity(0.9),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'All Stored Words',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Total: ${dictionary.count()}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: dictionary.count(),
+                        itemBuilder: (context, index) {
+                          final entry = dictionary.getAllWords()[index];
+                          return ListTile(
+                            title: Text(
+                              entry.key,
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              entry.value,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              color: Colors.red,
+                              onPressed: () {
+                                _deleteWord(entry.key);
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          // 리스트 보기 버튼
+          Positioned(
+            right: 16,
+            bottom: 16,
+            child: FloatingActionButton(
+              onPressed: _toggleWordList,
+              child: Icon(isListVisible ? Icons.close : Icons.list),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
