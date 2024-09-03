1511f3b5533503be30b44f9992a0775a99b81f95
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-09-03 10:18:48 +0900

    feat: 달력부분 추가

--- flutterzero/lib/screens/design_screen.dart
+++ flutterzero/lib/screens/design_screen.dart
@@ -10,23 +10,52 @@ class DesignScreen extends StatefulWidget {
 class _DesignScreenState extends State<DesignScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Icon(Icons.people),
                 Icon(Icons.add),
               ],
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Monday 16"),
+                Row(
+                  spacing: 8,
+                  children: [
+                    const Text("TODAY"),
+                    Center(
+                      child: Container(
+                        width: 5.0, // 너비 5px
+                        height: 5.0, // 높이 5px
+                        decoration: const BoxDecoration(
+                          color: Color(0xFFB22581), // 빨간색
+                          shape: BoxShape.circle, // 원 모양
+                        ),
+                      ),
+                    ),
+                    const Text("17"),
+                    const Text("18"),
+                    const Text("19"),
+                    const Text("20"),
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
