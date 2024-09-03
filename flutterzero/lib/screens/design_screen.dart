eebbae025431154a14283f63b500851222d7be05
Sunghum Paik (Brian) <zerosial@gmail.com>
2024-09-03 14:32:53 +0900

    feat: 데이 셀렉트 디자인

--- flutterzero/lib/screens/design_screen.dart
+++ flutterzero/lib/screens/design_screen.dart
@@ -17,10 +17,11 @@ class _DesignScreenState extends State<DesignScreen> {
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: Column(
+            spacing: 10,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
               const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,95 +30,123 @@ class _DesignScreenState extends State<DesignScreen> {
                   Icon(Icons.add),
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 10,
-                children: [
-                  const Text(
-                    "Monday 16",
-                    style: TextStyle(
-                      color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.all(6),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "MONDAY 16",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
                     ),
-                  ),
-                  Row(
-                    spacing: 8,
-                    children: [
-                      const Text(
-                        "TODAY",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      Center(
-                        child: Container(
-                          width: 5.0, // 너비 5px
-                          height: 5.0, // 높이 5px
-                          decoration: const BoxDecoration(
-                            color: Color(0xFFB22581), // 빨간색
-                            shape: BoxShape.circle, // 원 모양
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        spacing: 12,
+                        children: [
+                          Row(
+                            spacing: 6,
+                            children: [
+                              const Text(
+                                "TODAY",
+                                style: TextStyle(
+                                  fontSize: 38,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Center(
+                                child: Container(
+                                  margin: const EdgeInsets.only(
+                                    top: 6,
+                                  ),
+                                  width: 8.0,
+                                  height: 8.0,
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xFFB22581),
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
+                          Text(
+                            "17",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "18",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "19",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "20",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "21",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 36,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        "17",
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      Text(
-                        "18",
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      Text(
-                        "19",
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      Text(
-                        "20",
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const MeetingCard(
-                    startHour: "11",
-                    startMinute: "30",
-                    endHour: "12",
-                    endMinute: "20",
-                    meetingTitle: "DESIGN MEETING",
-                    participants: ["Alex", "Helena", "Nana"],
-                    color: Color(0xFFFEF754),
-                  ),
-                  const MeetingCard(
-                    startHour: "12",
-                    startMinute: "35",
-                    endHour: "14",
-                    endMinute: "00",
-                    meetingTitle: "DAILY PROJECT",
-                    participants: ["ME", "RICHARD", "CIRY", "+4"],
-                    color: Color(0xFF9C6BCE),
-                  ),
-                  const MeetingCard(
-                    startHour: "15",
-                    startMinute: "00",
-                    endHour: "16",
-                    endMinute: "30",
-                    meetingTitle: "WEEKLY PLANNING",
-                    participants: ["Den", "NANA", "MARK"],
-                    color: Color(0xFFBCEE4B),
-                  )
-                ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const MeetingCard(
+                startHour: "11",
+                startMinute: "30",
+                endHour: "12",
+                endMinute: "20",
+                meetingTitle: "DESIGN MEETING",
+                participants: ["Alex", "Helena", "Nana"],
+                color: Color(0xFFFEF754),
+              ),
+              const MeetingCard(
+                startHour: "12",
+                startMinute: "35",
+                endHour: "14",
+                endMinute: "00",
+                meetingTitle: "DAILY PROJECT",
+                participants: ["ME", "RICHARD", "CIRY", "+4"],
+                color: Color(0xFF9C6BCE),
+              ),
+              const MeetingCard(
+                startHour: "15",
+                startMinute: "00",
+                endHour: "16",
+                endMinute: "30",
+                meetingTitle: "WEEKLY PLANNING",
+                participants: ["Den", "NANA", "MARK"],
+                color: Color(0xFFBCEE4B),
               )
             ],
           ),
